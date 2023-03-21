5c5ff8f926573d6902a647c3e6491c9067b1505e
Михаил Олейник <mikhailoleynik@Mihails-MacBook-Pro.local>
2023-03-22 00:13:56 +0300

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
+
+mkdir data
+mkdir checkpoints
+
 cd data
 wget https://www.openslr.org/resources/12/train-clean-100.tar.gz
 tar –xvzf train-clean-100.tar.gz
 rm train-clean-100.tar.gz
+
+pip install -r requirements.txt
